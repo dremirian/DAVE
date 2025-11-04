af652df98ec4257df12d27ce81630ec25f8f4dec
Andressa Mirian <dre.mirian@hotmail.com>
2025-11-04 08:52:26 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -81,7 +81,7 @@ function App() {
           </h1>
           <p className="text-2xl text-cyan-400 mb-2">Database Assistant Virtual Engine</p>
           <p className="text-gray-300 max-w-3xl mx-auto text-lg">
-            Painel multifuncional para administração, análise e migração de bancos de dados, uma central inteligente e ágil para DBAs e desenvolvedores.
+            Painel multifuncional para administração, análise e migração de bancos de dados, uma central inteligente e ágil para DBAs.
           </p>
         </div>
 
